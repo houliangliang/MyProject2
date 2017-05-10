9da86273a91919aceb029cad72bf7a5a2b0bc931
HLL <510516199@q.com>
2017-05-10 21:48:50 +0800

    更新

--- myapplication/src/main/java/com/example/myapplication/Myradius.java
+++ myapplication/src/main/java/com/example/myapplication/Myradius.java
@@ -2,6 +2,10 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 public class Myradius extends AppCompatActivity  {
 
@@ -24,68 +28,68 @@ public class Myradius extends AppCompatActivity  {
 
     }
 
-//    public boolean onTouch(View v, MotionEvent event) {
-//        float x = event.getX();
-//        float y = event.getY();
-//        int action = event.getAction();
-//        switch (action) {
-//            case MotionEvent.ACTION_DOWN:
-//                break;
-//            case MotionEvent.ACTION_MOVE:
-//                break;
-//            case MotionEvent.ACTION_UP:
-//                //      钟表
-////                if (x > 130 && y > 180 && x < 250  && y < 250) {
-////                    Toast.makeText(MainActivity.this,"点击了",Toast.LENGTH_SHORT).show();
-////                }
-//
-//                //圆方
-//
-//                //圆心坐标
-//                int lastX = mFg.getWidth() / 2;
-//                int lastY = mFg.getHeight() / 2;
-//
-//                Log.i("zzz", "x  y " + x + "  " + y + " last x y " + lastX + " " + lastY);
-//
-//                //圆半径 通过左右坐标计算获得getLeft
-//                int r = (mFg.getRight() - mFg.getLeft()) / 2 - 5;
-//
-//                //点击位置x坐标与圆心的x坐标的距离
-//                int distanceX = Math.abs((int) x - lastX);
-//                //点击位置y坐标与圆心的y坐标的距离
-//                int distanceY = Math.abs((int) y - lastY);
-//                //点击位置与圆心的直线距离
-//                int distanceZ = (int) Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
-//
-//                //如果点击位置与圆心的距离小于圆的半径，证明点击位置在圆内
-//                if (distanceZ < r) {
-//
-//                    float x1 = x - lastX;
-//                    float y1 = (float) Math.floor((double) (y - lastY));
-//
-//                    if (r - Math.abs(x1) > Math.abs(y1)) {
-//
-//                        if (x > lastX - r / 2 && x < lastX + r / 2 && y > lastY - r / 2 && y < lastY + r / 2) {
-//                            Toast.makeText(Myradius.this, "在方内", Toast.LENGTH_SHORT).show();
-//                            return true;
-//                        }
-//
-//
-//                        Toast.makeText(Myradius.this, "在菱形内", Toast.LENGTH_SHORT).show();
-//                        return true;
-//                    }
-//
-//
-//                    Toast.makeText(Myradius.this, "在圆内", Toast.LENGTH_SHORT).show();
-//
+    public boolean onTouch(View v, MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                //      钟表
+//                if (x > 130 && y > 180 && x < 250  && y < 250) {
+//                    Toast.makeText(MainActivity.this,"点击了",Toast.LENGTH_SHORT).show();
 //                }
-//
-//
-//                break;
-//        }
-//        return true;
-//
-//    }
+
+                //圆方
+
+                //圆心坐标
+                int lastX = mFg.getWidth() / 2;
+                int lastY = mFg.getHeight() / 2;
+
+                Log.i("zzz", "x  y " + x + "  " + y + " last x y " + lastX + " " + lastY);
+
+                //圆半径 通过左右坐标计算获得getLeft
+                int r = (mFg.getRight() - mFg.getLeft()) / 2 - 5;
+
+                //点击位置x坐标与圆心的x坐标的距离
+                int distanceX = Math.abs((int) x - lastX);
+                //点击位置y坐标与圆心的y坐标的距离
+                int distanceY = Math.abs((int) y - lastY);
+                //点击位置与圆心的直线距离
+                int distanceZ = (int) Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+
+                //如果点击位置与圆心的距离小于圆的半径，证明点击位置在圆内
+                if (distanceZ < r) {
+
+                    float x1 = x - lastX;
+                    float y1 = (float) Math.floor((double) (y - lastY));
+
+                    if (r - Math.abs(x1) > Math.abs(y1)) {
+
+                        if (x > lastX - r / 2 && x < lastX + r / 2 && y > lastY - r / 2 && y < lastY + r / 2) {
+                            Toast.makeText(Myradius.this, "在方内", Toast.LENGTH_SHORT).show();
+                            return true;
+                        }
+
+
+                        Toast.makeText(Myradius.this, "在菱形内", Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+
+
+                    Toast.makeText(Myradius.this, "在圆内", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+                break;
+        }
+        return true;
+
+    }
 
 //    private void initView() {
 //        myview = (MyView) findViewById(R.id.myview);
